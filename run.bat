1f683b42e58dc9890de1d6c56cde5b2d5840b92a
Poof <poofy38@gmail.com>
2023-04-18 19:53:23 -0500

    Update

--- run.bat
+++ run.bat
@@ -2,15 +2,9 @@
 call activate Pytorch_1-12-1
 setlocal
 
-echo ==ME621==
-
-
-:loop
 for %%I in ("%~dp0main.py") do (
     python "%%~fI"
-    timeout /t 86400 /nobreak
 )
-goto loop
 
-pause
 call deactivate
+exit
